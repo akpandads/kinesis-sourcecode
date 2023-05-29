3a44a687a433467a24da3c2c4c22dabf53500c40
akpandads <39168913+akpandads@users.noreply.github.com>
2023-05-30 00:35:05 +0530

    Update KinesisSDKProducer.java
    removed unused import

--- src/main/java/com/akpanda/kinesis/producer/sdk/KinesisSDKProducer.java
+++ src/main/java/com/akpanda/kinesis/producer/sdk/KinesisSDKProducer.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.core.SdkBytes;
-
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.List;
